c16674d0d219ca100734cbd9efae711e35f27231
not-ed <43544402+not-ed@users.noreply.github.com>
2021-02-18 23:19:15 +0000

    Added basic image drawing.

--- notMarioBros/notMarioBros/main.cpp
+++ notMarioBros/notMarioBros/main.cpp
@@ -8,6 +8,12 @@ using namespace std;
 
 SDL_Window* g_window = nullptr;
 
+SDL_Renderer* g_renderer = nullptr;
+SDL_Texture* g_texture = nullptr;
+void Render();
+SDL_Texture* LoadTextureFromFile(string path);
+void FreeTexture();
+
 bool InitSDL();
 void CloseSDL();
 bool Update();
@@ -21,6 +27,7 @@ int main(int argc, char* args[]) {
 
 		// Entering game loop
 		while (!quit) {
+			Render();
 			quit = Update();
 		}
 	}
@@ -48,12 +55,41 @@ bool InitSDL() {
 			cerr << "[!] SDL window was not created. Error: " << SDL_GetError() << endl;
 			return false;
 		}
+
+		// Create renderer
+		g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
+		
+		if (g_renderer != nullptr) {
+			int imageFlags = IMG_INIT_PNG;
+
+			if (!(IMG_Init(imageFlags) & imageFlags)) {
+				cerr << "[!] SDL_Image could not be initialized. Error: " << IMG_GetError() << endl;
+				return false;
+			}
+		}
+		else { // Failed to create renderer
+			cerr << "[!] Renderer could not be initialized. Error: " << SDL_GetError() << endl;
+			return false;
+		}
+
+		// Load test texture
+		g_texture = LoadTextureFromFile("Images/test.bmp");
+		if (g_texture == nullptr) {
+			return false;
+		}
+
 	}
 	
 	return true;
 }
 
 void CloseSDL() {
+	//Clear Texture
+	FreeTexture();
+	// Release Renderer
+	SDL_DestroyRenderer(g_renderer);
+	g_renderer = nullptr;
+
 	// Release window and free memory
 	SDL_DestroyWindow(g_window);
 	g_window = nullptr;
@@ -76,11 +112,59 @@ bool Update() {
 		return true;
 		break;
 	case SDL_KEYDOWN:
-		if (e.key.keysym.scancode == SDL_SCANCODE_Q) {
+		if (e.key.keysym.sym == SDLK_q) {
 			return true;
 		}
 		break;
 	}
 
 	return false;
+}
+
+void Render() {
+	// Clear screen
+	SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, 0);
+	SDL_RenderClear(g_renderer);
+
+	SDL_Rect renderLocation = { 0,0,SCREEN_WIDTH,SCREEN_HEIGHT };
+
+	// Render to screen
+	SDL_RenderCopyEx(g_renderer, g_texture, NULL, &renderLocation, 0, NULL, SDL_FLIP_NONE);
+
+	// Update Screen
+	SDL_RenderPresent(g_renderer);
+}
+
+SDL_Texture* LoadTextureFromFile(string path) {
+	// Remove memory used for previous texture
+	FreeTexture();
+
+	SDL_Texture* p_texture = nullptr;
+
+	// Load image
+	SDL_Surface* p_surface = IMG_Load(path.c_str());
+	if (p_surface != nullptr)
+	{
+		// Create texture from the pixels found on the surface
+		p_texture = SDL_CreateTextureFromSurface(g_renderer, p_surface);
+		if (p_texture == nullptr) {
+			cerr << "[!] Unable to create texture from surface. Error: " << SDL_GetError() << endl;
+		}
+
+		// The loaded surface is no longer needed
+		SDL_FreeSurface(p_surface);
+	}
+	else {
+		cerr << "[!] Unable to create texture from surface. Error: " << IMG_GetError() << endl;
+	}
+
+	return p_texture;
+}
+
+void FreeTexture() {
+	// Check texture actually exists first.
+	if (g_texture != nullptr) {
+		SDL_DestroyTexture(g_texture);
+		g_texture = nullptr;
+	}
 }
